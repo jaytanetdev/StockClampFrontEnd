bda6ba10a3be24c842fb8a33085dc876ff89ec98
jaytanetdev <jaytanetdev@gmail.com>
2025-05-12 23:04:29 +0700

    fix: remove next export

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
 };
 
 export default nextConfig;
