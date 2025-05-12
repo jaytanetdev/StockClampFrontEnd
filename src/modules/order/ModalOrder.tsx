aa8bece3271f2bf463102f5fdade544dabdf1381
jaytanetdev <jaytanetdev@gmail.com>
2025-05-12 18:26:34 +0700

    fix: name

--- src/modules/order/ModalOrder.tsx
+++ src/modules/order/ModalOrder.tsx
@@ -405,7 +405,7 @@ const ModalOrder = (prop: ModalOrderType) => {
             </div>
 
             <SelectCustom
-              label="Product"
+              label="asdasdas"
               name="productId"
               placeholder="Product"
               onChange={handleProduct}
@@ -418,7 +418,7 @@ const ModalOrder = (prop: ModalOrderType) => {
               }}
             />
             <InputCustom
-              label="Amount"
+              label="asdasdasdas"
               name="amount"
               type="number"
               placeholder="amount"
