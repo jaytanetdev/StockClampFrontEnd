24eacf8ebd478f9e4e0ae8e9bfd93d8e5ab39421
jaytanetdev <jaytanetdev@gmail.com>
2025-05-12 22:54:32 +0700

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-    images: {
-    unoptimized: true, // << สำคัญมากสำหรับ static export
-  },
 };
 
 export default nextConfig;
